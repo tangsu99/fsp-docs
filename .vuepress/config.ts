2119b64f5d09ad98d734cf59bd1005d8676f418d
BusyPacket <1920909109@qq.com>
2025-08-20 18:27:57 +0800

    优化信息

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,115 +1,120 @@
 import { defineUserConfig } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
-import { viteBundler } from '@vuepress/bundler-vite'
-import { webpackBundler } from '@vuepress/bundler-webpack'
+import { viteBundler } from "@vuepress/bundler-vite";
+import { webpackBundler } from "@vuepress/bundler-webpack";
 
 export default defineUserConfig({
-  title: "FSP",
-  description: "文档站",
-  bundler: viteBundler(),
-  // bundler: webpackBundler(),
-  theme: recoTheme({
-    logo: "/serverlogo.png",
-    author: "fsp",
-    authorAvatar: "/serverlogo.png",
-    docsRepo: "https://github.com/tangsu99/fsp-docs",
-    docsBranch: "main",
-    docsDir: "/",
-    lastUpdatedText: "",
-    // series 为原 sidebar
-    series: {
-      "/docs/theme-reco/": [
-        {
-          text: "module one",
-          children: ["home", "theme"],
+    title: "FSP",
+    description: "文档站",
+    bundler: viteBundler(),
+    // bundler: webpackBundler(),
+    theme: recoTheme({
+        logo: "/serverlogo.png",
+        author: "fsp",
+        authorAvatar: "/serverlogo.png",
+        docsRepo: "https://github.com/tangsu99/fsp-docs",
+        docsBranch: "main",
+        docsDir: "/",
+        lastUpdatedText: "",
+        // series 为原 sidebar
+        series: {
+            "/docs/theme-reco/": [
+                {
+                    text: "module one",
+                    children: ["home", "theme"],
+                },
+                {
+                    text: "module two",
+                    children: ["api", "plugin"],
+                },
+            ],
         },
-        {
-          text: "module two",
-          children: ["api", "plugin"],
-        },
-      ],
-    },
-    navbar: [
-      { text: "主页", link: "/" },
-      { text: "分类", link: "/categories/fsp/1.html" },
-      { text: "标签", link: "/tags/fsp/1.html" },
-      { text: "关于", link: "/blogs/fsp/about.html" },
-      {
-        text: "文档",
-        children: [
-          { text: "生电模组介绍", link: "/docs/wiki/生电模组介绍.html" },
-          { text: "Carpet-FSP-addition", link: "/docs/wiki/CarpetFSPaddition.html" },
-          { text: "Carpet-FSP-addition-commands", link: "/docs/wiki/CarpetFSPadditionCommands.html" },
+        navbar: [
+            { text: "主页", link: "/" },
+            { text: "分类", link: "/categories/fsp/1.html" },
+            { text: "标签", link: "/tags/fsp/1.html" },
+            { text: "关于", link: "/blogs/fsp/about.html" },
+            {
+                text: "文档",
+                children: [
+                    {
+                        text: "服务器规则手册",
+                        link: "/docs/wiki/规则手册.html",
+                    },
+                    {
+                        text: "生电模组介绍",
+                        link: "/docs/wiki/生电模组介绍.html",
+                    },
+                ],
+            },
         ],
-      },
-    ],
-    // bulletin: {
-    //   body: [
-    //     {
-    //       type: "text",
-    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "QQ 群",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li>QQ群1：1037296104</li>
-    //         <li>QQ群2：1061561395</li>
-    //         <li>QQ群3：962687802</li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "GitHub",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "buttongroup",
-    //       children: [
-    //         {
-    //           text: "打赏",
-    //           link: "/docs/others/donate.html",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
-    // commentConfig: {
-    //   type: 'valine',
-    //   // options 与 1.x 的 valineConfig 配置一致
-    //   options: {
-    //     // appId: 'xxx',
-    //     // appKey: 'xxx',
-    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
-    //     // verify: true, // 验证码服务
-    //     // notify: true,
-    //     // recordIP: true,
-    //     // hideComments: true // 隐藏评论
-    //   },
-    // },
-  }),
-  // debug: true,
+        // bulletin: {
+        //   body: [
+        //     {
+        //       type: "text",
+        //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
+        //       style: "font-size: 12px;",
+        //     },
+        //     {
+        //       type: "hr",
+        //     },
+        //     {
+        //       type: "title",
+        //       content: "QQ 群",
+        //     },
+        //     {
+        //       type: "text",
+        //       content: `
+        //       <ul>
+        //         <li>QQ群1：1037296104</li>
+        //         <li>QQ群2：1061561395</li>
+        //         <li>QQ群3：962687802</li>
+        //       </ul>`,
+        //       style: "font-size: 12px;",
+        //     },
+        //     {
+        //       type: "hr",
+        //     },
+        //     {
+        //       type: "title",
+        //       content: "GitHub",
+        //     },
+        //     {
+        //       type: "text",
+        //       content: `
+        //       <ul>
+        //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+        //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+        //       </ul>`,
+        //       style: "font-size: 12px;",
+        //     },
+        //     {
+        //       type: "hr",
+        //     },
+        //     {
+        //       type: "buttongroup",
+        //       children: [
+        //         {
+        //           text: "打赏",
+        //           link: "/docs/others/donate.html",
+        //         },
+        //       ],
+        //     },
+        //   ],
+        // },
+        // commentConfig: {
+        //   type: 'valine',
+        //   // options 与 1.x 的 valineConfig 配置一致
+        //   options: {
+        //     // appId: 'xxx',
+        //     // appKey: 'xxx',
+        //     // placeholder: '填写邮箱可以收到回复提醒哦！',
+        //     // verify: true, // 验证码服务
+        //     // notify: true,
+        //     // recordIP: true,
+        //     // hideComments: true // 隐藏评论
+        //   },
+        // },
+    }),
+    // debug: true,
 });
--- docs/wiki/README.md
+++ docs/wiki/README.md
@@ -1,5 +1,6 @@
 # 导航
 
-- [生电模组介绍](/docs/wiki/生电模组介绍.md)
-- [CarpetFSPaddition](/docs/wiki/CarpetFSPaddition.md)
-- [CarpetFSPadditionCommands](/docs/wiki/CarpetFSPadditionCommands.md)
+-   [服务器规则](/docs/wiki/规则手册.md)
+-   [生电模组介绍](/docs/wiki/生电模组介绍.md)
+-   [CarpetFSPaddition](/docs/wiki/CarpetFSPaddition.md)
+-   [CarpetFSPadditionCommands](/docs/wiki/CarpetFSPadditionCommands.md)
